8d59da260126ba9994c2919ed5d03fba5a8b95ca
MohammedSendi <xmx115@gmail.com>
2022-08-06 02:43:41 +0300

    poll sorted by timestamp

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,20 +7,28 @@ const Home = (props) => {
         <h3>Answered</h3>
         <ul>
             {
-                props.answeredQuestionIds.map((id) => (
-                    <li key={id}> 
-                        <Question id={id}/>
-                    </li>
+                props.questions.map((question) => (
+                    props.answeredQuestionIds.map(id => {
+                        if(question.id === id){
+                            return <li key={id}> 
+                                <Question id={id}/>
+                            </li>
+                        }
+                    })
                 ))
             }
         </ul>
         <h3>Unanswered</h3>
         <ul>
             {
-                props.unAnsweredQuestionIds.map((id) => (
-                    <li key={id}> 
-                        <Question id={id}/>
-                    </li>
+                props.questions.map((question) => (
+                    props.unAnsweredQuestionIds.map(id => {
+                        if(question.id === id){
+                            return <li key={id}> 
+                                <Question id={id}/>
+                            </li>
+                        }
+                    })
                 ))
             }
         </ul>
@@ -29,7 +37,8 @@ const Home = (props) => {
 
 const mapStateToProps = ({users, questions, authedUser}) => ({
     answeredQuestionIds: Object.keys(users[authedUser].answers),
-    unAnsweredQuestionIds: Object.keys(questions).filter ((q) => !Object.keys(users[authedUser].answers).includes(q))
+    unAnsweredQuestionIds: Object.keys(questions).filter ((q) => !Object.keys(users[authedUser].answers).includes(q)),
+    questions: Object.values(questions).sort((a,b) => b.timestamp - a.timestamp)
 })
 
 export default connect(mapStateToProps)(Home);
